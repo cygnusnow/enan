f08c279c1b2c9edc99cce50ba79b1aec976b6d58
cygnus <ruohuyang@163.com>
2015-05-23 00:51:52 +0800

    add enan.py

--- ENAN.py
+++ ENAN.py
@@ -1,9 +1,20 @@
 #!/bin/python
 import time
+from datetime import date
 import math
 import tushare as ts
 import pandas as pd
 import numpy as np
 
+def enanSaveData2Excel(df):
+    t = date.today()
+    print t
+    fileName = r"F:\\stock\\ENAN\\data\\" + str(t) + r".xlsx"
+    df.to_excel(fileName)
+
+
+df = ts.get_tick_data('600848',date='2015-05-23')
+df.head(10)
+enanSaveData2Excel(df)
 
 
