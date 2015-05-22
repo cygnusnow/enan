40cddd7d2c632cd8669833b8f26e011fd234cc8f
cygnus <ruohuyang@163.com>
2015-05-23 01:02:16 +0800

    save to excel

--- ENAN.py
+++ ENAN.py
@@ -6,15 +6,8 @@ import tushare as ts
 import pandas as pd
 import numpy as np
 
-def enanSaveData2Excel(df):
-    t = date.today()
-    print t
-    fileName = r"F:\\stock\\ENAN\\data\\" + str(t) + r".xlsx"
-    df.to_excel(fileName)
 
-
-df = ts.get_tick_data('600848',date='2015-05-23')
-df.head(10)
-enanSaveData2Excel(df)
+df = ts.get_hist_data('000875')
+df.to_excel('F:/stock/ENAN/data/test.xlsx')
 
 
